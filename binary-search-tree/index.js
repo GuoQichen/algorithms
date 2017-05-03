f0263549e795a021a96b6ad7c2c865d5fc195cf6
guoqichen <250915274@qq.com>
2017-05-03 23:43:35 +0800

    add binary-search-tree folder

---
+++ binary-search-tree/index.js
@@ -0,0 +1,36 @@
+/**
+ * @author acky.guo
+ * @date 17/5/3
+ */
+
+class BinarySearchTree {
+	constructor() {
+		this._root = null
+	}
+
+	add(value) {
+
+	}
+
+	contains(value) {
+
+	}
+
+	remove(value) {
+
+	}
+
+	size() {
+
+	}
+
+	toArray() {
+
+	}
+
+	toString() {
+
+	}
+}
+
+module.exports = new BinarySearchTree()
---
+++ test/binary-search-tree.js
@@ -0,0 +1,11 @@
+const assert = require('assert')
+const b = require('../binary-search-tree')
+
+describe('binary-search-tree functional', function() {
+	it('#b.contains()')
+	it('#b.add()')
+	it('#b.size()')
+	it('#b.toArray()')
+	it('#b.toString()')
+	it('#b.remove()')
+})
