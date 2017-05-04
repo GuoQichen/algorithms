a37b5b2ddef56489aaf0f5de1058304d8a86483c
guoqichen <250915274@qq.com>
2017-05-04 22:02:32 +0800

    add binary search tree function and test case

--- binary-search-tree/index.js
+++ binary-search-tree/index.js
@@ -9,27 +9,84 @@ class BinarySearchTree {
 	}
 
 	add(value) {
-
+		const node = {
+			value,
+			left: null,
+			right: null
+		}
+		if(this._root === null) {
+			this._root = node
+			return
+		}
+		let current = this._root			
+		while(true) {
+			if(value < current.value) {
+				if(current.left === null) {
+					current.lefe = node
+				} else {
+					current = current.left
+				}
+			} else if(value > current.value) {
+				if(current.right === null) {
+					current.right = node
+				} else {
+					current = current.right
+				}
+			} else {
+				break
+			}
+		}
 	}
 
 	contains(value) {
-
+		const current = this._root
+		let found = false
+		while(current) {
+			if(value < current.value) { 
+				current = current.left
+			} else if(value > current.value) {
+				current = current.right
+			} else {
+				found = true
+				break
+			} 
+		}
+		return found
 	}
 
 	remove(value) {
 
 	}
 
-	size() {
+	_traverse(handler) {
+		if(this._root === null) throw new Error('root element is null')
+		const current = this._root
+		const call = (node) => {
+			if(node.left) call(node.left)
+			handler.call(this, node)
+			if(node.right) call(node.right)
+		}
+		call(current)
+	}
 
+	size() {
+		let count = 0
+		this._traverse(() => {
+			count++
+		})
+		return count
 	}
 
 	toArray() {
-
+		const arr = []
+		this._traverse(({ value }) => {
+			arr.push(value)
+		})
+		return arr
 	}
 
 	toString() {
-
+		return this.toArray().toString()
 	}
 }
 
--- test/binary-search-tree.js
+++ test/binary-search-tree.js
@@ -2,10 +2,46 @@ const assert = require('assert')
 const b = require('../binary-search-tree')
 
 describe('binary-search-tree functional', function() {
-	it('#b.contains()')
-	it('#b.add()')
-	it('#b.size()')
-	it('#b.toArray()')
-	it('#b.toString()')
+	beforeEach('reset binary-search-tree root node to null', function() {
+		b._root = null
+	})
+	it('#b.contains()', function() {
+		assert(!b.contains(1))
+		b.add(1)
+		assert(b.contains(1))
+	})
+	it('#b.add()', function() {
+		assert(b._root === null)
+		b.add(1)
+		assert.deepEqual(b._root, {
+			value: 1,
+			left: null,
+			right: null
+		})
+	})
+	it('#b.size()', function() {
+		assert.throws(() => {
+			b.size()
+		}, /root element is null/)
+		b.add(1)
+		assert.equal(b.size(), 1)
+		b.add(2)
+		b.add(3)
+		assert.equal(b.size(), 3)
+	})
+	it('#b.toArray()', function() {
+		b.add(1)
+		b.add(2)
+		b.add(3)
+		const arr = b.toArray()
+		assert.deepEqual(arr, [1, 2, 3])
+	})
+	it('#b.toString()', function() {
+		b.add(1)
+		b.add(2)
+		b.add(3)
+		const str = b.toString()
+		assert(str === '1,2,3')
+	})
 	it('#b.remove()')
 })
